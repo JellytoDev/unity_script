0f0f3865bdfd5cf7cb0a96996149c13db2dc59f9
JellytoDev <wjdwhdgus2222@naver.com>
2021-11-24 01:20:11 +0900

    firebase connet and delete item

---
+++ DeleteItem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DataInfo;
+using Firebase.Firestore;
+using Firebase.Extensions;
+using Firebase.Storage;
+using System;
+using UnityEngine.Networking;
+
+
+public class DeleteItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject idObj;
+    public GameObject kindObj;
+
+    string email = "wjdwhdgus222@naver.com";
+
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Debug.Log("Delete Button Click");
+
+            string kind = kindObj.GetComponent<Text>().text;
+            string id = idObj.GetComponent<Text>().text;
+
+            Debug.Log(kind);
+            Debug.Log(id);
+
+            FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+
+            DocumentReference itemRef = db.Collection("users").Document(email).Collection(kind).Document(id);
+            itemRef.DeleteAsync();
+
+            Destroy(this.transform.parent.gameObject);
+            
+
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
