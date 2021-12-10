4d613fa13788a812ff82c279663adef2e2a76d99
JellytoDev <wjdwhdgus2222@naver.com>
2021-12-11 01:35:50 +0900

    point charge

---
+++ PointCharge.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointCharge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string email = "wjdwhdgus222@naver.com"; //DataInfo.User.email 
+        this.GetComponent<Button>().onClick.AddListener(() =>
+        {
+
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
