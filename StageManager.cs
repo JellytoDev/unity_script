dbb7609762fea71e1fe6d5f142c5851ad2df8798
JellytoDev <wjdwhdgus2222@naver.com>
2021-12-18 13:52:01 +0900

    stage management script

---
+++ StageManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageManager : MonoBehaviour {
+
+	public GameObject characterPrefab;
+	public Transform spawnPos;
+
+	public static GameObject myCharacter;
+
+	// Use this for initialization
+	void Start () {
+
+		characterPrefab.transform.localScale = new Vector3(1, 1, 1);
+	// instantiate girl character on the spawn position
+		myCharacter = Instantiate( characterPrefab, spawnPos.position, spawnPos.rotation ) as GameObject;
+		myCharacter.transform.position = new Vector3(1, 0, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
